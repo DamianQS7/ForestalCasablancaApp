da5cfbfb921bde3a0ff83846d8fa81cf737c0dfe
DamianQS7 <102097286+DamianQS7@users.noreply.github.com>
2023-06-29 12:50:52 -0400

    Initial commit: ProjectStructure and Packages

---
+++ ForestalCasablancaApp/Pages/MainPage.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace ForestalCasablancaApp;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+}
+
---
+++ ForestalCasablancaApp/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace ForestalCasablancaApp;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
