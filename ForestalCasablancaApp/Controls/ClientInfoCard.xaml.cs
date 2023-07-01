6e6d556466585af8b3776529f75129b978addef1
DamianQS7 <102097286+DamianQS7@users.noreply.github.com>
2023-07-01 19:32:25 -0400

    Add basic layout for the pages. Created ClientInfoCard custom control

---
+++ ForestalCasablancaApp/Controls/ClientInfoCard.xaml.cs
@@ -0,0 +1,9 @@
+namespace ForestalCasablancaApp.Controls;
+
+public partial class ClientInfoCard : ContentView
+{
+	public ClientInfoCard()
+	{
+		InitializeComponent();
+	}
+}
--- ForestalCasablancaApp/Pages/LeñaPage.xaml.cs
+++ ForestalCasablancaApp/Pages/LeñaPage.xaml.cs
@@ -2,7 +2,6 @@ using ForestalCasablancaApp.ViewModels;
 
 namespace ForestalCasablancaApp.Pages;
 
-[QueryProperty(nameof(LeñaViewModel), nameof(LeñaViewModel))]
 public partial class LeñaPage : ContentPage
 {
 	public LeñaPage(LeñaViewModel viewModel)
