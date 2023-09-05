b599d115ee72fb7ff775c7630fb70badfa9e68b5
DamianQS7 <102097286+DamianQS7@users.noreply.github.com>
2023-09-04 20:12:19 -0400

    Added ThemePicker in SettingsPage

--- ForestalCasablancaApp/App.xaml.cs
+++ ForestalCasablancaApp/App.xaml.cs
@@ -1,4 +1,6 @@
-﻿namespace ForestalCasablancaApp;
+﻿using ForestalCasablancaApp.Helpers;
+
+namespace ForestalCasablancaApp;
 
 public partial class App : Application
 {
@@ -6,6 +8,33 @@ public partial class App : Application
 	{
 		InitializeComponent();
 
+		ThemePicker.SetTheme();
+
 		MainPage = new AppShell();
+	}
+
+	protected override void OnStart()
+	{
+		OnResume();
+    }
+
+	protected override void OnSleep()
+	{
+		ThemePicker.SetTheme();
+		RequestedThemeChanged -= AppRequestedThemeChanged;
+    }
+
+	protected override void OnResume()
+	{
+		ThemePicker.SetTheme();
+		RequestedThemeChanged += AppRequestedThemeChanged;
+	}
+
+	private void AppRequestedThemeChanged(object sender, AppThemeChangedEventArgs e)
+	{
+		MainThread.BeginInvokeOnMainThread(() =>
+		{
+			ThemePicker.SetTheme();
+		});
 	}
 }
---
+++ ForestalCasablancaApp/Pages/SettingsPage.xaml.cs
@@ -0,0 +1,62 @@
+using ForestalCasablancaApp.Helpers;
+
+namespace ForestalCasablancaApp.Pages;
+
+public partial class SettingsPage : ContentPage
+{
+	public SettingsPage()
+	{
+		InitializeComponent();
+
+        // Set the correct RadioButton when the app is launched.
+		switch (Settings.Theme)
+		{
+            case 0:
+                RadBtnSystem.IsChecked = true;
+                break;
+            case 1:
+                RadBtnLight.IsChecked = true;
+                break;
+            case 2:
+                RadBtnDark.IsChecked = true;
+                break;
+        }
+	}
+
+    bool isLoaded;
+
+    protected override void OnAppearing()
+    {
+        base.OnAppearing();
+        isLoaded = true;
+    }
+
+    void RadioButton_CheckedChanged(object sender, CheckedChangedEventArgs e)
+    {
+        if (!isLoaded)
+            return;
+
+        if (!e.Value)
+            return;
+
+        var val = (sender as RadioButton)?.Value as string;
+
+        if(string.IsNullOrEmpty(val))
+            return;
+
+        switch(val)
+        {
+            case "System":
+                Settings.Theme = 0;
+                break;
+            case "Light":
+                Settings.Theme = 1;
+                break;
+            case "Dark":
+                Settings.Theme = 2;
+                break;
+        }
+
+        ThemePicker.SetTheme();
+    }
+}
