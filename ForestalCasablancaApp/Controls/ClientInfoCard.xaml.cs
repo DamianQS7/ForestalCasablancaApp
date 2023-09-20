50ff0fc6774791f68ca0c6ece631ad6d767f8c6f
DamianQS7 <102097286+DamianQS7@users.noreply.github.com>
2023-09-20 01:23:46 -0400

    Finished requested modifications on TrozoAserrablePage

--- ForestalCasablancaApp/Controls/ClientInfoCard.xaml.cs
+++ ForestalCasablancaApp/Controls/ClientInfoCard.xaml.cs
@@ -8,9 +8,6 @@ public partial class ClientInfoCard : ContentView
     public static readonly BindableProperty RutClienteProperty =
         BindableProperty.Create(nameof(RutCliente), typeof(string), typeof(ClientInfoCard), default, BindingMode.TwoWay);
 
-    public static readonly BindableProperty PatenteClienteProperty =
-        BindableProperty.Create(nameof(PatenteCliente), typeof(string), typeof(ClientInfoCard), default, BindingMode.TwoWay);
-
 
     public string NombreCliente
     {
@@ -24,17 +21,10 @@ public partial class ClientInfoCard : ContentView
         set => SetValue(RutClienteProperty, value);
     }
 
-    public string PatenteCliente
-    {
-        get => (string)GetValue(PatenteClienteProperty);
-        set => SetValue(PatenteClienteProperty, value);
-    }
-
     public ClientInfoCard()
     {
         InitializeComponent();
         NombreEntry.SetBinding(Entry.TextProperty, new Binding(nameof(NombreCliente), source: this));
         RutEntry.SetBinding(Entry.TextProperty, new Binding(nameof(RutCliente), source: this));
-        PatenteEntry.SetBinding(Entry.TextProperty, new Binding(nameof(PatenteCliente), source: this));
     }
 }
--- ForestalCasablancaApp/Pages/TrozoAserrablePage.xaml.cs
+++ ForestalCasablancaApp/Pages/TrozoAserrablePage.xaml.cs
@@ -10,4 +10,21 @@ public partial class TrozoAserrablePage : ContentPage
 
 		BindingContext = viewModel;
 	}
+
+	//void OnPickerSelectedIndexChanged(object sender, EventArgs e)
+	//{
+ //       var picker = (Picker)sender;
+ //       int selectedIndex = picker.SelectedIndex;
+
+ //       if (selectedIndex == 0 || selectedIndex == 1)
+	//	{
+	//		_largos = new() { "3,2", "4", "5", "6", "7" };
+ //       }
+	//	else if(selectedIndex == 2 || selectedIndex == 3)
+	//	{
+			
+	//	}
+ //   }
+
+	//private List<string> _largos;
 }
