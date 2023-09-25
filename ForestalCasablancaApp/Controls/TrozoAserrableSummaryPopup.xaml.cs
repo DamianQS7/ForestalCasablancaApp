0999699f96f4fb0fb3412935f523d7488b45090e
DamianQS7 <102097286+DamianQS7@users.noreply.github.com>
2023-09-25 02:34:37 -0400

    Finished SummaryPopup UI and Logic. Next task is the PDF

---
+++ ForestalCasablancaApp/Controls/TrozoAserrableSummaryPopup.xaml.cs
@@ -0,0 +1,11 @@
+using CommunityToolkit.Maui.Views;
+
+namespace ForestalCasablancaApp.Controls;
+
+public partial class TrozoAserrableSummaryPopup : Popup
+{
+	public TrozoAserrableSummaryPopup()
+	{
+		InitializeComponent();
+	}
+}
